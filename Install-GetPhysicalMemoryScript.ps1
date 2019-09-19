090d9c3f6cab487a93536fe68cc764ccbe410639
sorcia25 <50998375+sorcia25@users.noreply.github.com>
2019-09-19 08:49:29 -0500

    Upload File
    Execute this script as an system administrator. This script will download the Get-PhysicalMemory.ps1 from GitHub, create a self signed certificate, and signing the code with that certificate to doing the execution without changing the execution policy of the system.

---
+++ Install-GetPhysicalMemoryScript.ps1
@@ -0,0 +1,5 @@
+﻿## Script para crear certificado autofirmado para Code Signing y firmar el codigo para obtener la memoria de forma segura
+$path = "C:\Windows\System32\drivers\etc\Get-PhysicalMemory.ps1"
+wget https://raw.githubusercontent.com/sorcia25/PublicScripts/master/Get-PhysicalMemory.ps1 -OutFile $path
+$cert1 = New-SelfSignedCertificate -KeyUsage DigitalSignature -CertStoreLocation "Cert:\CurrentUser\My" -DnsName PSCodeSigning -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3")
+Set-AuthenticodeSignature -Certificate:$cert1 -FilePath $path
