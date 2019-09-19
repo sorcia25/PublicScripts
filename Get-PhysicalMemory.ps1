be8ad14b139ad462b4526e28b24817bec84754cb
sorcia25 <50998375+sorcia25@users.noreply.github.com>
2019-09-19 17:23:09 -0500

    Update Get-PhysicalMemory.ps1

--- Get-PhysicalMemory.ps1
+++ Get-PhysicalMemory.ps1
@@ -1,10 +1,11 @@
 $preRegPath = 'HKLM:\SYSTEM\'
 $RegKey = 'TotalPhysicalMemory'
 $registrypath = $preRegPath+$RegKey
-
+# Get memory data
 $Memory1 = (systeminfo | Select-String 'Total Physical Memory:').ToString().Split(':')[1].Trim()
 $Memory2 = $Memory1.Replace('MB','')
 $Memory3 = $Memory2.Replace(',','')
 $TotalMemory = [int]$Memory3
+# Create the Registry property
 New-Item -Path HKLM:\SYSTEM\ -Name TotalPhysicalMemory -Force
 New-ItemProperty -Path HKLM:\SYSTEM\TotalPhysicalMemory -Name MemoryinMB -PropertyType String -Value $TotalMemory
