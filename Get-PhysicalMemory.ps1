30249c96754ebf1f41b18eefb7cc576f74cc8da3
sorcia25 <50998375+sorcia25@users.noreply.github.com>
2019-09-19 14:19:29 -0500

    Update Get-PhysicalMemory.ps1

--- Get-PhysicalMemory.ps1
+++ Get-PhysicalMemory.ps1
@@ -7,5 +7,5 @@ $Memory2 = $Memory1.Replace('MB','')
 $Memory3 = $Memory2.Replace(',','')
 $TotalMemory = [int]$Memory3
 
-New-Item -Path HKLM:\SYSTEM\ -Name TotalPhysicalMemory –Force
-Set-Item -Path HKLM:\SYSTEM\TotalPhysicalMemory -Value $TotalMemory -Type String –Force
+New-Item -Path HKLM:\SYSTEM\ -Name TotalPhysicalMemory -Force
+Set-Item -Path HKLM:\SYSTEM\TotalPhysicalMemory -Value $TotalMemory -Type String -Force
