baba70a08481cbcbb5baded5d504c8c069307db4
sorcia25 <50998375+sorcia25@users.noreply.github.com>
2019-09-19 14:06:19 -0500

    Update Get-PhysicalMemory.ps1
    Change the script to push the value to a registry instead a file...

--- Get-PhysicalMemory.ps1
+++ Get-PhysicalMemory.ps1
@@ -1,7 +1,11 @@
-﻿$filepath = 'C:\Windows\System32\drivers\etc\avmemory.log'
-if (Test-Path -Path $filepath) {Remove-Item $filepath}
+$preRegPath = 'HKLM:\SYSTEM\'
+$RegKey = 'TotalPhysicalMemory'
+$registrypath = $preRegPath+$RegKey
+
 $Memory1 = (systeminfo | Select-String 'Total Physical Memory:').ToString().Split(':')[1].Trim()
 $Memory2 = $Memory1.Replace('MB','')
 $Memory3 = $Memory2.Replace(',','')
 $TotalMemory = [int]$Memory3
-$TotalMemory | Out-File -FilePath $filepath
+
+New-Item -Path HKLM:\SYSTEM\ -Name TotalPhysicalMemory –Force
+Set-Item -Path HKLM:\SYSTEM\TotalPhysicalMemory -Value $TotalMemory -Type String -Force
